df408a06b8b74fddfef93f67205840a5a2ddac9a
Joaquin Manuel Crespo <joaquincrespo96@gmail.com>
2022-01-31 17:43:46 +0000

    Update csv-ipinfo.sh

--- csv-ipinfo.sh
+++ csv-ipinfo.sh
@@ -5,52 +5,6 @@
 # LICENSE; GPL <https://www.gnu.org/licenses/gpl-3.0.html>
 
 # $1 = First argument = The CSV File with IPs separated with comas and newlines
-# $2 = The country to filter with "country" field
-#	https://www.uptodate.com/contents/image?imageKey=DRUG%2F116421 
-
-#	Country abbreviations used in Brand Names: International field of Lexicomp monographs
-#	Argentina (AR) 	Honduras (HN) 	Peru (PE)
-#	Australia (AU) 	Hong Kong (HK) 	Philippines (PH)
-#	Austria (AT) 	Hungary (HU) 	Poland (PL)
-#	Bahamas (BS) 	Iceland (IS) 	Portugal (PT)
-#	Bahrain (BH) 	India (IN) 	Puerto Rico (PR)
-#	Bangladesh (BD) 	Indonesia (ID) 	Qatar (QA)
-#	Barbados (BB) 	Iran (IR) 	Republic of Korea (KR)
-#	Belgium (BE) 	Iraq (IQ) 	Romania (RO)
-#	Belize (BZ) 	Ireland (IE) 	Russian Federation (RU)
-#	Benin (BJ) 	Israel (IL) 	Saudi Arabia (SA)
-#	Bermuda (BM) 	Italy (IT) 	Senegal (SN)
-#	Bolivia (BO) 	Jamaica (JM) 	Seychelles (SC)
-#	Brazil (BR) 	Japan (JP) 	Sierra Leone (SL)
-#	Bulgaria (BG) 	Jordan (JO) 	Singapore (SG)
-#	Burkina Faso (BF) 	Kenya (KE) 	Slovakia (SK)
-#	Chile (CL) 	Kuwait (KW) 	Slovenia (SI)
-#	China (CN) 	Latvia (LV) 	South Africa (ZA)
-#	Colombia (CO) 	Lebanon (LB) 	Spain (ES)
-#	Costa Rica (CR) 	Liberia (LR) 	Sri Lanka (LK)
-#	Côte D' Ivoire (CI) 	Libya (LY) 	Sudan (SD)
-#	Croatia (HR) 	Liechtenstein (LI) 	Surinam (SR)
-#	Cuba (CU) 	Lithuania (LT) 	Sweden (SE)
-#	Cyprus (CY) 	Luxembourg (LU) 	Switzerland (CH)
-#	Czech Republic (CZ) 	Malawi (MW) 	Syria (SY)
-#	Denmark (DK) 	Malaysia (MY) 	Syrian Arab Republic (SY)
-#	Dominican Republic (DO) 	Mali (ML) 	Taiwan, Province of China (TW)
-#	Ecuador (EC) 	Malta (MT) 	Tanzania (TZ)
-#	Egypt (EG) 	Mauritania (MR) 	Thailand (TH)
-#	El Salvador (SV) 	Mauritius (MU) 	Trinidad and Tobago (TT)
-#	Estonia (EE) 	Mexico (MX) 	Tunisia (TN)
-#	Ethiopia (ET) 	Morocco (MA) 	Turkey (TR)
-#	Finland (FI) 	Netherlands (NL) 	Uganda (UG)
-#	France (FR) 	New Zealand (NZ) 	Ukraine (UA)
-#	Gambia (GM) 	Nicaragua (NI) 	United Arab Emirates (AE)
-#	Germany (DE) 	Niger (NE) 	Uruguay (UY)
-#	Ghana (GH) 	Nigeria (NG) 	Venezuela (VE)
-#	Great Britain [UK] (GB) 	Norway (NO) 	Vietnam (VN)
-#	Greece (GR) 	Oman (OM) 	Yemen (YE)
-#	Guatemala (GT) 	Pakistan (PK) 	Zambia (ZM)
-#	Guinea (GN) 	Panama (PA) 	Zimbabwe (ZW)
-#	Guyana (GY) 	Paraguay (PY)
-
 
 while read ip
 do
